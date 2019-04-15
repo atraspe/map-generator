4d092249201a568e6cc57b4bb8f7a6aba3568167
Art <atraspe@yahoo.com>
2019-04-15 02:34:24 -0400

    Obtain information from Title sheet

--- map-gen.py
+++ map-gen.py
@@ -1,5 +1,3 @@
-# Import load_workbook from openpyxl module to handle 
-# how the maps specs is parsed (usually an Excel spreadsheet)
 from openpyxl import load_workbook
 
 # Load entire spreadsheet/workbook into this object
@@ -7,3 +5,13 @@ wb = load_workbook('Example_Spec_v1.0.xlsx')
 
 # Print all workbook sheets
 print(f'Sheets available:\n{wb.sheetnames}\n')
+
+# Obtain information from Title sheet
+title_sheet = wb['Title']
+
+for i in range(2, 11):
+    for j in range(1, 3):
+        print(title_sheet.cell(row=i, column=j).value, end=' ')
+    print()
+
+udf_sheet = wb['UDF']
